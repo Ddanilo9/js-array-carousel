554f8bed5e8b2d8fab1770a398ce4cf507325ec1
Ddanilo9 <interiordanilo3@gmail.com>
2022-07-26 15:58:07 +0200

    aggiunta eventi sulle frecce

--- js/slider.js
+++ js/slider.js
@@ -8,9 +8,13 @@ const slides = [
 	'./img/05.jpg',
 ]
 
+//pusho in questo nuovo array il contenuto
+let slideElements = []
+currentIndex = 0
 
 let containerSlider = document.querySelector('.slides-wrapper');
 containerSlider.innerHTML= '';
+
 // console.log(containerSlider)
 
 //contenuto slider
@@ -20,19 +24,52 @@ let src = slides[i] //racchiudo in una nuova variabile
 
 
 //creo l'elemento li
-let li = document.createElement('li')
+let li = document.createElement('li');
 //aggiungo classe a li
-li.className = ('slide')
+li.className = ('slide');
 
 //agiungo la classe active sul  primo elemento dell'array
 if (i === 0){
-	li.classList.add('active')
+	li.classList.add('active');
 }
 // creo l'elemento immagine
-let img = document.createElement('img')
+let img = document.createElement('img');
 img.src = src
 li.append(img)
 // console.log(src)
-containerSlider.append (li)
+containerSlider.append (li);
+slideElements.push(li);
 
 }
+
+console.log(slideElements)
+
+//prendo la freccia destra da dntro il dom
+let arrowRight = document.querySelector('.arrow-next')
+// console.log(arrowRight)
+
+arrowRight.addEventListener('click', function() {
+	let slideAttiva = slideElements[currentIndex]
+	// console.log(slideAttiva)
+	slideAttiva.classList.remove('active')
+
+	let slideSuccessiva = slideElements[currentIndex + 1]
+	slideSuccessiva.classList.add('active')
+
+	currentIndex ++
+})
+
+let arrowleft = document.querySelector('.arrow-prev')
+// console.log(arrowleft)
+
+arrowleft.addEventListener('click', function() {
+	let slideAttiva = slideElements[currentIndex]
+	// console.log(slideAttiva)
+	slideAttiva.classList.remove('active');
+
+	let slideSuccessiva = slideElements[currentIndex - 1]
+	slideSuccessiva.classList.add('active');
+
+	currentIndex --
+})
+
